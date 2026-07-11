00588c8b900f9e6b562c4e9c4efa7e4842594c53
Frank <frankwamuyu40@gmail.com>
2026-07-11 18:06:24 +0300

    how to work around arrays merge & loops

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require("datejs");
+function combineUsers(...args) {
+  combinedObject = {
+    users: [],
+  };
+  //loop throught the args then merge as it adds thhrought the length
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users = [...combinedObject.users, ...args[i]];
+  }
 
-
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
